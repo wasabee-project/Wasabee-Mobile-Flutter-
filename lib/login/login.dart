c993b09a0071fce418d7b608392aad192ae04939
Steven Foskett <steven.foskett@gmail.com>
2019-06-26 15:44:31 -0500

    Got the request to auth working with wasabee
    Also added handling the wasabee cookie.

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:convert' as convert;
+import 'package:dio/dio.dart';
+import 'package:cookie_jar/cookie_jar.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key, this.title}) : super(key: key);
@@ -13,6 +16,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String api = "https://server.wasabee.rocks";
+
   @override
   Widget build(BuildContext context) {
     String titleString = " - Authenticate";
@@ -24,11 +29,33 @@ class _LoginPageState extends State<LoginPage> {
           child: GoogleSignInButton(
               onPressed: () {
                 Auth(googleSignIn: GoogleSignIn()).signInWithGoogle().then(
-                    (GoogleSignInAuthentication googleAuth) => print('Hi $googleAuth'));
+                    (GoogleSignInAuthentication googleAuth) =>
+                        doLogin(googleAuth.accessToken));
               },
               darkMode: true),
         ));
   }
 
-  doLogin() {}
+  doLogin(String accessToken) async {
+    var url = api + "/aptok";
+    String sendData = convert.jsonEncode({
+      'accessToken': '$accessToken',
+    });
+
+    print('URL: $url');
+    print('sendData: $sendData');
+    try {
+      var dio = new Dio();
+      var cj = CookieJar();
+      var cm = CookieManager(cj);
+      dio.interceptors.add(cm);
+
+      Response response = await dio.post(url, data: sendData);
+      var cookieList = cj.loadForRequest(Uri.parse(url));
+      print('cookies are -> $cookieList');
+      print(response);
+    } catch (e) {
+      print(e);
+    }
+  }
 }
